c588290d228a3467c62f40c1d08cb533672aeab8
Phil White <phil.white@creditsafe.com>
2021-03-18 15:21:42 +0000

    Add some tests and fetching options

--- src/App.js
+++ src/App.js
@@ -1,5 +1,52 @@
+import { useState } from "react";
+import axios from "axios";
+import { ajax } from "rxjs/ajax";
+import { map } from "rxjs/operators";
+
+const fetchPokemon = (pokemonName) =>
+  fetch("https://pokeapi.co/api/v2/pokemon/" + pokemonName).then((r) =>
+    r.json()
+  );
+
+const fetchPokemonAxios = (pokemonName) =>
+  axios
+    .get("https://pokeapi.co/api/v2/pokemon/" + pokemonName)
+    .then((r) => Promise.resolve(r.data));
+
+const fetchPokemonRxjs = (pokemonName) =>
+  ajax
+    .getJSON("https://pokeapi.co/api/v2/pokemon/" + pokemonName)
+    .pipe(map((response) => response))
+    .toPromise();
+
 function App() {
-  return <div className="App"></div>;
+  const [pokemon, setPokemon] = useState({});
+
+  const onSubmit = (pokemonName) =>
+    fetchPokemonRxjs(pokemonName).then((data) => {
+      setPokemon(data);
+    });
+
+  return (
+    <div className="App">
+      <h1>Poké Search</h1>
+      <form
+        onSubmit={(e) => {
+          e.preventDefault();
+          onSubmit(e.target.elements.name.value);
+        }}
+      >
+        <label htmlFor="search">Search</label>
+        <input id="search" name="name" type="text" />
+      </form>
+
+      <div>Name: {pokemon.name}</div>
+      <img
+        src={pokemon.sprites?.front_default}
+        alt={pokemon.name + "-sprite"}
+      />
+    </div>
+  );
 }
 
 export default App;
--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,34 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import { fireEvent, render, screen, waitFor } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
+import App from "./App";
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+describe("Poke Search App", () => {
+  test("Main heading text is visible", () => {
+    render(<App />);
+    const heading = screen.getByRole("heading", { name: /Poké search/i });
+    expect(heading).toBeInTheDocument();
+  });
+
+  test.each([
+    [
+      "pikachu",
+      "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/25.png",
+    ],
+    [
+      "raichu",
+      "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/26.png",
+    ],
+  ])("Can search for %s", async (pokemon, src) => {
+    render(<App />);
+
+    expect(screen.queryByText(`Name: ${pokemon}`)).not.toBeInTheDocument();
+
+    const searchBox = screen.getByLabelText(/Search/i);
+    userEvent.type(searchBox, pokemon + "{enter}");
+
+    expect(await screen.findByText(`Name: ${pokemon}`)).toBeInTheDocument();
+
+    const sprite = screen.getByAltText(pokemon + "-sprite");
+    expect(sprite).toHaveAttribute("src", src);
+  });
 });
