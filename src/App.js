cf833760e1e0aa60963418e3c129d86b3da58e50
Phil White <phil.white@creditsafe.com>
2021-03-25 08:18:25 +0000

    Cleanup again for demo

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import { useState } from "react";
-import { Grid, Typography, TextField } from "@material-ui/core";
 import { PokemonSummary } from "./PokemonSummary";
 import { fetchPokemonRxjs, fetchPokemonAxios, fetchPokemon } from "./api";
 
