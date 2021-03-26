81e179763a85dee086f9215a5f23fad048864a0a
Phil White <phil.white@creditsafe.com>
2021-03-26 08:18:50 +0000

    Comment initial tests for speed

--- src/App.test.js
+++ src/App.test.js
@@ -8,37 +8,37 @@ describe("Poke Search App", () => {
     render(<App />);
   });
 
-  test.skip.each`
-    pokemon      | src
-    ${"pikachu"} | ${"https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/25.png"}
-    ${"raichu"}  | ${"https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/26.png"}
-  `("Can search for $pokemon and view sprite", async ({ pokemon, src }) => {
-    render(<App />);
-  });
+  // test.skip.each`
+  //   pokemon      | src
+  //   ${"pikachu"} | ${"https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/25.png"}
+  //   ${"raichu"}  | ${"https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/26.png"}
+  // `("Can search for $pokemon and view sprite", async ({ pokemon, src }) => {
+  //   render(<App />);
+  // });
 
-  test.skip("Displays error message when the API fails", async () => {
-    server.use(
-      rest.get(
-        "https://pokeapi.co/api/v2/pokemon/pikachu",
-        async (req, res, ctx) => {
-          return res(ctx.status(500));
-        }
-      )
-    );
-    render(<App />);
+  // test("Displays error message when the API fails", async () => {
+  //   server.use(
+  //     rest.get(
+  //       "https://pokeapi.co/api/v2/pokemon/pikachu",
+  //       async (req, res, ctx) => {
+  //         return res(ctx.status(500));
+  //       }
+  //     )
+  //   );
+  //   render(<App />);
 
-    const searchBox = screen.getByLabelText(/Search/i);
-    userEvent.type(searchBox, "pikachu{enter}");
+  //   const searchBox = screen.getByLabelText(/Search/i);
+  //   userEvent.type(searchBox, "pikachu{enter}");
 
-    expect(await screen.findByText(`Oops I'm broken!`)).toBeInTheDocument();
-  });
+  //   expect(await screen.findByText(`Oops I'm broken!`)).toBeInTheDocument();
+  // });
 
-  test.skip("Displays not found message when pokemon does not exist", async () => {
-    render(<App />);
+  // test("Displays not found message when pokemon does not exist", async () => {
+  //   render(<App />);
 
-    const searchBox = screen.getByLabelText(/Search/i);
-    userEvent.type(searchBox, "dave{enter}");
+  //   const searchBox = screen.getByLabelText(/Search/i);
+  //   userEvent.type(searchBox, "dave{enter}");
 
-    expect(await screen.findByText(`Not a pokemon`)).toBeInTheDocument();
-  });
+  //   expect(await screen.findByText(`Not a pokemon`)).toBeInTheDocument();
+  // });
 });
